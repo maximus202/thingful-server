8b5e9fe1014c1681b37753c8309a056e739b0f00
Max R <maxx.ratto@gmail.com>
2020-02-27 07:35:12 -0700

    Work done until data protection checkpoint. JWT Module, checkpoint 4.

--- seeds/seed.thingful_tables.sql
+++ seeds/seed.thingful_tables.sql
@@ -8,12 +8,12 @@ TRUNCATE
 
 INSERT INTO thingful_users (user_name, full_name, nickname, password)
 VALUES
-  ('dunder', 'Dunder Mifflin', null, 'password'),
-  ('b.deboop', 'Bodeep Deboop', 'Bo', 'bo-password'),
-  ('c.bloggs', 'Charlie Bloggs', 'Charlie', 'charlie-password'),
-  ('s.smith', 'Sam Smith', 'Sam', 'sam-password'),
-  ('lexlor', 'Alex Taylor', 'Lex', 'lex-password'),
-  ('wippy', 'Ping Won In', 'Ping', 'ping-password');
+  ('dunder', 'Dunder Mifflin', null, '$2a$12$QHbOhtejgHfvG6ywlJc3pextBN.YwLgyPdKQAlu/7av3Z/ZFNhlE6'),
+  ('b.deboop', 'Bodeep Deboop', 'Bo', '$2a$12$sZb8KiCKjp3OpAjadJR/6ef4vY6vh4SLgP3vIrWLmaKQT/qSwFpOW'),
+  ('c.bloggs', 'Charlie Bloggs', 'Charlie', '$2a$12$s/gUCopRE3rb9mbZeFki.u713GnekqpvZx1LjaU5W.J0jdfcOxj0.'),
+  ('s.smith', 'Sam Smith', 'Sam', '$2a$12$Xth3NOX2aCGMMvkLPu7fgOfvVNSHVNFk/Y8K23vWcn08lLAaJ3V0.'),
+  ('lexlor', 'Alex Taylor', 'Lex', '$2a$12$9s76wRDv23pSNjjZuSpA3Ozpvdz6soSj8IQS1ouyRHam7h7VF62Py'),
+  ('wippy', 'Ping Won In', 'Ping', '$2a$12$Aj4Y8YjeyVkuaZkj.CJmFe13X6OZ1xzhUM5jC9h12VuykxfERhU8O');
 
 INSERT INTO thingful_things (title, image, user_id, content)
 VALUES
--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+
 function makeUsersArray() {
   return [
     {
@@ -133,7 +135,7 @@ function makeReviewsArray(users, things) {
   ];
 }
 
-function makeExpectedThing(users, thing, reviews=[]) {
+function makeExpectedThing(users, thing, reviews = []) {
   const user = users
     .find(user => user.id === thing.user_id)
 
@@ -162,7 +164,7 @@ function makeExpectedThing(users, thing, reviews=[]) {
 }
 
 function calculateAverageReviewRating(reviews) {
-  if(!reviews.length) return 0
+  if (!reviews.length) return 0
 
   const sum = reviews
     .map(review => review.rating)
@@ -230,7 +232,21 @@ function cleanTables(db) {
   )
 }
 
-function seedThingsTables(db, users, things, reviews=[]) {
+function seedUsers(db, users) {
+  const preppedUsers = users.map(user => ({
+    ...user,
+    password: bcrypt.hashSync(user.password, 1)
+  }))
+  return db.into('thingful_users').insert(preppedUsers)
+    .then(() =>
+      db.raw(
+        `SELECT setval('thingful_users_id_seq', ?)`,
+        [users[users.length - 1].id],
+      )
+    )
+}
+
+function seedThingsTables(db, users, things, reviews = []) {
   return db
     .into('thingful_users')
     .insert(users)
@@ -245,14 +261,17 @@ function seedThingsTables(db, users, things, reviews=[]) {
 }
 
 function seedMaliciousThing(db, user, thing) {
-  return db
-    .into('thingful_users')
-    .insert([user])
+  return seedUsers(db, [user])
     .then(() =>
       db
         .into('thingful_things')
         .insert([thing])
     )
+}
+
+function makeAuthHeader(user) {
+  const token = Buffer.from(`${user.user_name}:${user.password}`).toString('base64')
+  return `Basic ${token}`
 }
 
 module.exports = {
@@ -262,9 +281,10 @@ module.exports = {
   makeExpectedThingReviews,
   makeMaliciousThing,
   makeReviewsArray,
-
   makeThingsFixtures,
   cleanTables,
   seedThingsTables,
   seedMaliciousThing,
+  makeAuthHeader,
+  seedUsers
 }
